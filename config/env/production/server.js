15f394cbc6e9fb13e65781de74e69259c0490e71
Federico Senmartin <fede.senmartin@gmail.com>
2022-09-19 19:54:56 -0300

    no message

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,7 @@
 
 module.exports = ({ env }) => ({
-    url: env('MY_HEROKU_URL'),
-  });
-  
+  url: env('MY_HEROKU_URL'),
+  app: {
+    keys: ['bb7aba1f-db8c-4267-8149-91a9e9386ae3', 'bb7aba1f-db8c-4267-8149-91a9e9386ae3']
+  },
+});
