4d11e559a86bf1d7785c88abd84b4ec041b490bc
Federico Senmartin <fede.senmartin@gmail.com>
2022-09-19 19:16:35 -0300

    test

--- config/database.js
+++ config/database.js
@@ -1,13 +1,19 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'app'),
-      user: env('DATABASE_USERNAME', 'db_username'),
-      password: env('DATABASE_PASSWORD', 'db_password'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
   },
 });
